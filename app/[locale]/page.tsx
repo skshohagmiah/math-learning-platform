ff28ddbd553f9669c0b42546a030d60040e549e7
shohag <shohagmiah2100@gmail.com>
2024-07-15 21:11:59 +0600

    layout changed

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,14 @@
 import ChapterIntroduction from "@/components/chapters/ChapterIntroduction";
 import Hero from "@/components/homepage/HeroSection";
 import HowItWorks from "@/components/homepage/HowItWork";
-import {mathCurriculum} from '@/messages/mathCurriculum.json'
+
 
 
 export default function Home() {
   return (
     <main className="bg-gradient-to-br from-purple-50 to-indigo-100 dark:from-gray-900 dark:to-indigo-900">
       <Hero />
-      <ChapterIntroduction chapters={mathCurriculum!} />
+      <ChapterIntroduction />
       <HowItWorks />
     </main>
   );
